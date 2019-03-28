92bcc42489c49c82e63d3fb3b2bff11bd71c1c6c
kobelb <brandon.kobel@elastic.co>
2019-03-28 09:40:25 -0700

    Thrasing around

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+var SecretClass = require('secret-repo').SecretClass;
+var instance = new SecretClass();
+console.log(instance.dude());
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const { SecretClass } = require('secret-repo');
+const instance = new SecretClass();
+console.log(instance.dude());
